ecf877bf0ecedcd94d376df454979689cb197429
ingridcriistine <ingridrochacristine@gmail.com>
2024-12-06 14:36:26 -0300

    começando

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ export default function TabLayout() {
 
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{title: 'Home',}} />
+      <Tabs.Screen name="index" options={{title: 'Home', }} />
       <Tabs.Screen name="explore" options={{title: 'Explore',}} />
     </Tabs>
   );
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Button, Text, Image, StyleSheet, View } from 'react-native';
+
+export default function Login() {
+    return(
+        <div>
+            <Image source={require('../assets/images/calda.png')} style={styles.img}/>
+        </div>
+    );
+}
+
+const styles = StyleSheet.create({
+    img: {
+      width: 200,
+    }
+  });
