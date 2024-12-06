4b0a7cdf3a8873818769ddd77f7442cd1c18dc6a
xmarimarquesh <marimarquesh39@gmail.com>
2024-12-06 14:44:19 -0300

    ta

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '@/components/RootLayout';
 
-
 export default function Home(){
     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
     const irTabs = () => {
