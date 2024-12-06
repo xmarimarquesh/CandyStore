51660c57329537043169ff3cc92df39ae04443cd
ingridcriistine <ingridrochacristine@gmail.com>
2024-12-06 14:42:57 -0300

    updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button, Text, Image, StyleSheet, View } from 'react-native';
 
 export default function Login() {
@@ -12,20 +11,8 @@ export default function Login() {
 const styles = StyleSheet.create({
     img: {
       width: 200,
+
     }
   });
-=======
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { RootStackParamList } from '@/components/RootLayout';
 
 
-export default function Home(){
-    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-    const irTabs = () => {
-        navigation.navigate("(tabs)");
-    }
-    return (
-        <button onClick={irTabs}>OK</button>
-    )
-}
->>>>>>> dev
